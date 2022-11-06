9446da53aff63cbe4a2f78fda218408201c275c9
unknown <megakuul@gmx.ch>
2022-11-06 12:26:32 +0100

    Navbar created

--- src/script.js
+++ src/script.js
@@ -1,2 +1,11 @@
-console.log("Hallo Wello");
+function getNavItems() {
+}
+function setActive(id) {
+    var navbar = document.getElementsByClassName("active");
+    for (var i = 0; i < navbar.length; i++) {
+        navbar.item(i).classList.remove("active");
+    }
+    var item = document.getElementById(id);
+    item.classList.add("active");
+}
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log("Hallo Wello");
+function getNavItems() {
+    
+}
+
+function setActive(id) {
+    let navbar = document.getElementsByClassName("active");
+    for (let i = 0; i < navbar.length; i++)
+    {
+        navbar.item(i).classList.remove("active");
+    }
+    
+    let item = document.getElementById(id);
+    item.classList.add("active");
+}
+
