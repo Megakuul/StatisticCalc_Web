ba9f238361ced642f8d099c5548fd5e5c3a432d6
unknown <megakuul@gmx.ch>
2022-11-13 22:01:03 +0100

    Dialogbox created; Backdrop blur

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import * as DialogBox from "./extern/DialogBox.js";
-const body_id = "body";
+const addBx_id = "addBx";
+const removeBx_id = "removeBx";
 const add_class = "addvalue";
 const remove_class = "removevalue";
 const navbar_class = "navbar";
@@ -7,9 +7,6 @@ const navitem_class = "navitem";
 const active_class = "active";
 let valueList;
 let currentActive = document.getElementsByClassName(active_class)[0];
-let removeItemBox = "<p>Hallloooo Welt</p>";
-//Onload create the Dialog
-DialogBox.createDialog("body", removeItemBox);
 initEventListeners();
 function initEventListeners() {
     //Init Navitems
@@ -17,6 +14,32 @@ function initEventListeners() {
     for (let i = 0; i < navbar.length; i++) {
         navbar.item(i).addEventListener("click", setActive(navbar.item(i)));
     }
+    let addValueElements = document.getElementsByClassName(add_class);
+    for (let i = 0; i < addValueElements.length; i++) {
+        addValueElements.item(i).addEventListener("click", showDialog(addBx_id));
+    }
+    let removeValueElements = document.getElementsByClassName(remove_class);
+    for (let i = 0; i < addValueElements.length; i++) {
+        removeValueElements.item(i).addEventListener("click", showDialog(removeBx_id));
+    }
+    window.addEventListener("click", function (event) {
+        if (event.target == document.getElementById(addBx_id))
+            document.getElementById(addBx_id).style.display = "none";
+    });
+    window.addEventListener("click", function (event) {
+        if (event.target == document.getElementById(removeBx_id))
+            document.getElementById(removeBx_id).style.display = "none";
+    });
+}
+function showDialog(id) {
+    return function () {
+        document.getElementById(id).style.display = "block";
+    };
+}
+function hideDialog(id) {
+    return function () {
+        document.getElementById(id).style.display = "none";
+    };
 }
 function setActive(element) {
     return function () {
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,5 @@
-import * as DialogBox from "./extern/DialogBox.js";
-
-const body_id = "body";
+const addBx_id = "addBx";
+const removeBx_id = "removeBx";
 
 const add_class = "addvalue";
 const remove_class = "removevalue";
@@ -12,14 +11,7 @@ let valueList;
 
 let currentActive = document.getElementsByClassName(active_class)[0];
 
-let removeItemBox = "<p>Hallloooo Welt</p>"
-
-//Onload create the Dialog
-DialogBox.createDialog("body", removeItemBox)
-
 initEventListeners();
-
-
 
 
 
@@ -27,10 +19,41 @@ function initEventListeners() {
     //Init Navitems
     let navbar = document.getElementsByClassName(navitem_class);
     for (let i = 0; i < navbar.length; i++) {
-        navbar.item(i).addEventListener("click", setActive(navbar.item(i)))
+        navbar.item(i).addEventListener("click", setActive(navbar.item(i)));
+    }
+
+    let addValueElements = document.getElementsByClassName(add_class);
+    for (let i = 0; i < addValueElements.length; i++) {
+        addValueElements.item(i).addEventListener("click", showDialog(addBx_id));
+    }
+
+    let removeValueElements = document.getElementsByClassName(remove_class);
+    for (let i = 0; i < addValueElements.length; i++) {
+        removeValueElements.item(i).addEventListener("click", showDialog(removeBx_id));
+    }
+
+    window.addEventListener("click", function(event) {
+        if (event.target == document.getElementById(addBx_id))
+            document.getElementById(addBx_id).style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == document.getElementById(removeBx_id))
+            document.getElementById(removeBx_id).style.display = "none";
+    });
+}
+
+function showDialog(id: string) {
+    return function() {
+        document.getElementById(id).style.display = "block";
     }
 }
 
+function hideDialog(id) {
+    return function() {
+        document.getElementById(id).style.display = "none";
+    }
+}
 
 function setActive(element) {
     return function() {
