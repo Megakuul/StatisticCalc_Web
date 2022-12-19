e3281a6aadea69924d3cd71e48e7d90ec2df936d
unknown <megakuul@gmx.ch>
2022-12-19 22:11:19 +0100

    Chart configured and styled (alignment is not correct right now)

--- src/dist/chart.js
+++
@@ -1,3 +0,0 @@
-export function createChart(chart_id) {
-}
-//# sourceMappingURL=chart.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,8 @@
-import * as Chart from 'chart.js';
+//Chart.js Lib
+import "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js";
+
+//Make that Typescript compiler shut the fuck up
+declare var Chart: any;
 
 const addBx         = <HTMLDivElement>$("addBx");
 const removeBx      = <HTMLDivElement>$("removeBx");
@@ -19,21 +23,53 @@ const navbar_class = "navbar";
 const navitem_class = "navitem";
 const active_class = "active";
 
-let valueList = [];
+let valueListY = [];
+
+let valueListX = [];
 
 let currentActive = $$(active_class)[0];
 
+//Chart------------------------------------------------------------------//
+
+    const chart = new Chart(mainChart, {
+        type: "line",
+        data: {
+            labels: [],
+            datasets: [{
+                label: "Values",
+                data: [],
+                borderColor: 'rgba(255, 255, 255, 0.8)',
+                borderWidth: 1,
+                fill: true
+            }]
+        },
+
+        options: {
+            scales: {
+                xAxes: [{
+                    ticks: {
+                      fontColor: 'rgba(255, 255, 255, 0.8)'
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                      fontColor: 'rgba(255, 255, 255, 0.8)'
+                    }
+                }],
+            },
+            legend: {
+                labels: {
+                    fontColor: 'rgba(255, 255, 255, 0.8)',
+                }
+            }
+        }
+    });
+
+//Chart------------------------------------------------------------------//
+
+
+
 initEventListeners();
-
-//shortener for getElementBy -> anti boilerplate
-function $(input: string) {
-    return document.getElementById(input);
-}
-
-function $$(input: string) {
-    return document.getElementsByClassName(input);
-}
-//shortener for getElementBy -> anti boilerplate
 
 function initEventListeners() {
     //Init Navitems
@@ -88,8 +124,8 @@ function initEventListeners() {
     });
 
     clearListBtn.addEventListener("click", function() {
-        valueList = [];
-        rewrapList();
+        valueListY = [];
+        rewrap();
     });
 }
 
@@ -135,9 +171,9 @@ function resetDialog(slider: HTMLInputElement, valBx: HTMLInputElement, countr: 
 function addValueToList(value, count) {
     if (!isNaN(value) && value != "") {
         for (let i = 0; i < count; i++) {
-            valueList.push(value);
+            valueListY.push(value);
         }
-        rewrapList();
+        rewrap();
         return true;
     } else {
         return false;
@@ -147,48 +183,55 @@ function addValueToList(value, count) {
 function removeValueFromList(value, count) {
     if (!isNaN(value)) {
         for (let i = 0; i < count; i++) {
-            const index = valueList.indexOf(value);
+            const index = valueListY.indexOf(value);
             if (index > -1) {
-                valueList.splice(index, 1);
+                valueListY.splice(index, 1);
             }
         }
-        rewrapList();
+        rewrap();
         return true;
     } else {
         return false;
     }
 }
 
+function rewrap() {
+    setXValue();
+    rewrapList();
+    rewrapChart();
+}
+
 function rewrapList() {
     let list = $("valueList");
     list.innerHTML = "";
 
-    for (let i = 0; i < valueList.length; i++) {
-        list.innerHTML += `<li class="${valueList[i]}">${valueList[i]}</li>`;
+    for (let i = 0; i < valueListY.length; i++) {
+        list.innerHTML += `<li class="${valueListY[i]}">${valueListY[i]}</li>`;
     }
 }
 
-function createChart() {
-    return new Chart("myChart", {
-        type: "line",
-        data: {
-          labels: xValues,
-          datasets: [{ 
-            data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-            borderColor: "red",
-            fill: false
-          }, { 
-            data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-            borderColor: "green",
-            fill: false
-          }, { 
-            data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-            borderColor: "blue",
-            fill: false
-          }]
-        },
-        options: {
-          legend: {display: false}
-        }
-    });
+function rewrapChart() {
+    chart.data.labels = valueListX;
+    chart.data.datasets[0].data = valueListY;
+    chart.update();
 }
+
+function setXValue() {
+    valueListX = [];
+    for (let i = 0; i < valueListY.length; i++) {
+        valueListX.push(i+1);
+    }
+    console.log(valueListX.length);
+}
+
+
+
+//shortener for getElementBy -> anti boilerplate
+function $(input: string) {
+    return document.getElementById(input);
+}
+
+function $$(input: string) {
+    return document.getElementsByClassName(input);
+}
+//shortener for getElementBy -> anti boilerplate
